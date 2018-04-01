605f73d0269b9e0111f681adff58e2105ecea637
rikenshah <rikenmanojshah@gmail.com>
2018-04-01 15:51:36 -0400

    removed unnecessary logins

--- config/auth.js
+++ config/auth.js
@@ -4,20 +4,20 @@ require('dotenv').config();
 // expose our config directly to our application using module.exports
 module.exports = {
 
-    'facebookAuth' : {
-        'clientID'        : process.env.GOOGLE_CLIENT_ID, // your App ID
-        'clientSecret'    : process.env.GOOGLE_CLIENT_SECRET, // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
-        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-        'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    // 'facebookAuth' : {
+    //     'clientID'        : process.env.GOOGLE_CLIENT_ID, // your App ID
+    //     'clientSecret'    : process.env.GOOGLE_CLIENT_SECRET, // your App Secret
+    //     'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+    //     'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+    //     'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
 
-    },
+    // },
 
-    'twitterAuth' : {
-        'consumerKey'        : process.env.GOOGLE_CLIENT_ID,
-        'consumerSecret'     : process.env.GOOGLE_CLIENT_SECRET,
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
-    },
+    // 'twitterAuth' : {
+    //     'consumerKey'        : process.env.GOOGLE_CLIENT_ID,
+    //     'consumerSecret'     : process.env.GOOGLE_CLIENT_SECRET,
+    //     'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+    // },
 
     'googleAuth' : {
         'clientID'         : process.env.GOOGLE_CLIENT_ID,
