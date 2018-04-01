c1769afdbc831a73cb15f106744f703bde1f38d3
rikenshah <rikenmanojshah@gmail.com>
2018-04-01 17:02:49 -0400

    Changed envs to include callback url

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'         : process.env.GOOGLE_CLIENT_ID,
         'clientSecret'     : process.env.GOOGLE_CLIENT_SECRET,
-        'callbackURL'      : 'http://findmeroommate.herokuapp.com/auth/google/callback'
+        'callbackURL'      : process.env.GOOGLE_CALLBACK_URL
     }
 
 };
