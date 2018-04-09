ab5cdaa409025bb0fe2da57a7d4a27c91c3de64d
AarohGala <aarohgala37@gmail.com>
2018-04-08 22:03:45 -0400

    Customised Euclidian

--- ml/kmeans.js
+++ ml/kmeans.js
@@ -12,7 +12,15 @@ function euclidianDistance(a, b)
   let d = 0.0;
   for (let i = 0, max = a.length; i < max; ++i) 
   {
-  	d += Math.pow((a[i] - b[i]), 2);
+  	// d += Math.pow((a[i] - b[i]), 2);
+  	if(i <= 8)
+  	{
+  		d += 0.9*(Math.pow((a[i] - b[i]), 2));	
+  	}
+  	else
+  	{
+  		d += 0.1*(Math.pow((a[i] - b[i]), 2));	
+  	}
   }
   return Math.sqrt(d);
 }
